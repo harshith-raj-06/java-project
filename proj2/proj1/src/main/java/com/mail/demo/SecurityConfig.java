cff30f63a857469b6fe497bad4fe99c81ad9be24
Bharath Vikas Tadepalli <bharath.tadepalli@iiitb.ac.in>
2025-11-22 21:20:50 +0530

    commit

---
+++ proj2/proj1/src/main/java/com/mail/demo/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.mail.demo;
+
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+import static org.springframework.aot.generate.ValueCodeGenerator.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.sessionManagement(session
+                -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        //http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}
---
+++ proj2/proj1/src/main/java/com/mail/demo/controllers/Importance.java
@@ -0,0 +1,6 @@
+package com.mail.demo.controllers;
+
+public enum Importance {
+    DEFAULT,  //1
+    IMPORTANT //2
+}
