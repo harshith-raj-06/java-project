25d39c85ca417cb893458faae621eb4a3db839b6
harshith-raj-06 <harshithraj2006@gmail.com>
2026-03-12 18:42:10 +0530

    Delete pranav/harshith directory

--- pranav/harshith/src/main/java/com/mail/demo/model/ScheduledMail.java
+++
@@ -1,9 +0,0 @@
-package com.mail.demo.model;
-
-public class ScheduledMail extends Mail{
-    private String scheduledTime;
-
-    public String getScheduledTime() { return scheduledTime; }
-    public void setScheduledTime(String t) { scheduledTime = t; }
-
-}
